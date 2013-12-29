cc8309d019d671efb90867976abfdbbb3448e5de
Mohammad Shamsi <m.h.shams@gmail.com>
2013-12-30 00:26:46 +0100

    added log service to bundle activator (low level).

--- bactv/src/main/java/my/sample/bactv/MyBundleActivator.java
+++ bactv/src/main/java/my/sample/bactv/MyBundleActivator.java
@@ -3,19 +3,33 @@ package my.sample.bactv;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.log.LogService;
 
 /**
  * @author mohammad shamsi <m.h.shams@gmail.com>
  */
 public class MyBundleActivator implements BundleActivator {
     @Override
-    public void start(BundleContext bundleContext) throws Exception {
-        System.out.println("\nStarting Bundle: (" + getInfo(bundleContext.getBundle()) + ")\n");
+    public void start(BundleContext context) throws Exception {
+        log(context, "Starting Bundle: (" + getInfo(context.getBundle()) + ")");
     }
 
     @Override
-    public void stop(BundleContext bundleContext) throws Exception {
-        System.out.println("\nStopping Bundle: (" + getInfo(bundleContext.getBundle()) + ")\n");
+    public void stop(BundleContext context) throws Exception {
+        log(context, "Stopping Bundle: (" + getInfo(context.getBundle()) + ")");
+    }
+
+    private void log(BundleContext context, String message) {
+        ServiceReference ref = context.getServiceReference(LogService.class.getName());
+
+        if (ref != null) {
+            LogService log = (LogService) context.getService(ref);
+            log.log(LogService.LOG_INFO, message);
+
+        } else {
+            System.out.println(message);
+        }
     }
 
     private String getInfo(Bundle bundle) {
