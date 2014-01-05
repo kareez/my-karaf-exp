cf12668dbe3506939d3d24e4c14139ed9e542e29
Mohammad Shamsi <m.h.shams@gmail.com>
2014-01-05 22:39:47 +0100

    added spock specifications to activator

---
+++ activator/src/test/groovy/my/sample/activator/ActivatorSpec.groovy
@@ -0,0 +1,52 @@
+package my.sample.activator
+
+import org.osgi.framework.Bundle
+import org.osgi.framework.BundleContext
+import org.osgi.framework.ServiceReference
+import org.osgi.framework.Version
+import org.osgi.service.log.LogService
+import spock.lang.Specification
+
+/**
+ * @author mohammad shamsi <m.h.shams@gmail.com>
+ */
+class ActivatorSpec extends Specification {
+    def context = Mock(BundleContext)
+    def service = Mock(LogService)
+    def bundle = Mock(Bundle)
+
+    def setup() {
+        bundle.getSymbolicName() >> "bundle-symbolic-name"
+        bundle.getVersion() >> new Version("1.0.0")
+
+        context.getServiceReference(_ as String) >> Mock(ServiceReference)
+        context.getService(_ as ServiceReference) >> service
+        context.getBundle() >> bundle
+    }
+
+    def "Start a bundle"() {
+        when:
+        new Activator().start(context)
+
+        then:
+        1 * service.log(LogService.LOG_WARNING, "Starting Bundle: (bundle-symbolic-name: 1.0.0)")
+        1 * context.getServiceReference(_ as String)
+//        1 * context.getService(_ as ServiceReference)
+//        1 * context.getBundle()
+//        1 * bundle.getVersion()
+//        1 * bundle.getSymbolicName()
+    }
+
+    def "Stop a bundle"() {
+        when:
+        new Activator().stop(context)
+
+        then:
+        1 * service.log(LogService.LOG_WARNING, "Stopping Bundle: (bundle-symbolic-name: 1.0.0)")
+//        1 * context.getServiceReference(_ as String)
+//        1 * context.getService(_ as ServiceReference)
+//        1 * context.getBundle()
+//        1 * bundle.getSymbolicName()
+//        1 * bundle.getVersion()
+    }
+}
---
+++ activator/src/test/groovy/my/sample/activator/impl/DefaultLoggerSpec.groovy
@@ -0,0 +1,51 @@
+package my.sample.activator.impl
+
+import org.osgi.service.log.LogService
+import spock.lang.Specification
+
+/**
+ * @author mohammad shamsi <m.h.shams@gmail.com>
+ */
+class DefaultLoggerSpec extends Specification {
+    DefaultLogger defaultLogger
+    LogService service
+
+    def setup() {
+        service = Mock(LogService)
+
+        defaultLogger = new DefaultLogger()
+        defaultLogger.logger = service
+    }
+
+    def "call info"() {
+        when:
+        defaultLogger.info("message")
+
+        then:
+        1 * service.log(LogService.LOG_INFO, "message")
+    }
+
+    def "call debug"() {
+        when:
+        defaultLogger.debug("message")
+
+        then:
+        1 * service.log(LogService.LOG_DEBUG, "message")
+    }
+
+    def "call warn"() {
+        when:
+        defaultLogger.warn("message")
+
+        then:
+        1 * service.log(LogService.LOG_WARNING, "message")
+    }
+
+    def "call error"() {
+        when:
+        defaultLogger.error("message")
+
+        then:
+        1 * service.log(LogService.LOG_ERROR, "message")
+    }
+}
