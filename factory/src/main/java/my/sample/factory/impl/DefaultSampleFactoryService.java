5b8406dd99d84b22bd5f4d7e458b09f91f9de969
Mohammad Shamsi <m.h.shams@gmail.com>
2014-02-01 21:14:14 +0100

    keep factory and stop here

--- factory/src/main/java/my/sample/factory/impl/DefaultSampleFactoryService.java
+++ factory/src/main/java/my/sample/factory/impl/DefaultSampleFactoryService.java
@@ -2,11 +2,15 @@ package my.sample.factory.impl;
 
 import my.sample.factory.SampleFactoryService;
 
+import java.util.Map;
+
 /**
  * @author mohammad shamsi <m.h.shams@gmail.com>
  */
 public class DefaultSampleFactoryService implements SampleFactoryService {
-    private InternalBean internalBean;
+    private String foo;
+    private String bar;
+    private String baz;
 
     public DefaultSampleFactoryService() {
         System.out.println("new DefaultSampleFactoryService: " + this);
@@ -15,19 +19,40 @@ public class DefaultSampleFactoryService implements SampleFactoryService {
     public void init() {
         System.out.println("Initializing DefaultSampleFactoryService: " + this);
     }
+
+    public void update(Map<String, ?> config) {
+        System.out.println("updating....: " + this);
+    }
+
+    public void destroy() {
+        System.out.println("Destroying DefaultSampleFactoryService: " + this);
+    }
+
     public String echo(String message) {
         return "Echo processed: " + message;
     }
 
-    public void setInternalBean(InternalBean internalBean) {
-        System.out.println("setting new internal bean: " + internalBean);
-        this.internalBean = internalBean;
+    public void setFoo(String foo) {
+        System.out.println("setting new foo:" + foo);
+        this.foo = foo;
+    }
+
+    public void setBar(String bar) {
+        System.out.println("setting new bar:" + bar);
+        this.bar = bar;
+    }
+
+    public void setBaz(String baz) {
+        System.out.println("setting new baz:" + baz);
+        this.baz = baz;
     }
 
     @Override
     public String toString() {
-        return "DefaultSampleFactoryService {" +
-                " internalBean = " + internalBean +
+        return " InternalBean {" +
+                " foo = '" + foo + '\'' +
+                ", bar = '" + bar + '\'' +
+                ", baz = '" + baz + '\'' +
                 " }";
     }
 }
