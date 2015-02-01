c37f1cd98a2fbaf61d6a4aa59ac026e37c7d362d
Mohammad Shamsi <m.h.shams@gmail.com>
2015-02-01 22:49:05 +0100

    updated to karaf 3.0.3.

--- provider/src/main/java/my/sample/provider/SampleDecorator.java
+++ provider/src/main/java/my/sample/provider/SampleDecorator.java
@@ -6,12 +6,9 @@ package my.sample.provider;
 public interface SampleDecorator {
     /**
      * Decorates the given message.
-     * the default implementation just return the string without any change.
      *
      * @param message the message to be decorated
      * @return the decorated message
      */
-    default String decorate(String message) {
-        return message;
-    }
+     String decorate(String message);
 }
